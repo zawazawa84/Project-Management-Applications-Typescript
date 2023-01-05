2800634b61f9c31d8bafeb2edba775a2981f07db
aizawa <taiki84taiki@gmail.com>
2023-01-05 21:13:41 +0900

    ユーザー入力の取得とバリデーション

--- app.js
+++ app.js
@@ -1,1 +1,110 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function validate(validatableInput) {
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null &&
+        typeof validatableInput.value === "string") {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null &&
+        typeof validatableInput.value === "string") {
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (validatableInput.min != null &&
+        typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (validatableInput.max != null &&
+        typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+}
+// autobind decorator
+function autobind(target, methodName, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+// ProjectInput class
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = "user-input";
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.mandayInputElement = this.element.querySelector("#manday");
+        this.configure();
+        this.attach();
+    }
+    gatherUserInput() {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredManday = this.mandayInputElement.value;
+        const titleValidatable = {
+            value: enteredTitle,
+            required: true
+        };
+        const descriptionValidatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        };
+        const mandayValidatable = {
+            value: enteredManday,
+            required: true,
+            min: 1,
+            max: 100
+        };
+        if (!validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(mandayValidatable)) {
+            alert("入力値が正しくありません。再度お試しください。");
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDescription, enteredManday];
+        }
+    }
+    clearInputs() {
+        this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.mandayInputElement.value = "";
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, manday] = userInput;
+            console.log(title, desc, manday);
+            this.clearInputs();
+        }
+    }
+    configure() {
+        this.element.addEventListener("submit", this.submitHandler);
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+__decorate([
+    autobind
+], ProjectInput.prototype, "submitHandler", null);
+const prjInput = new ProjectInput();
--- app.ts
+++ app.ts
@@ -1,1 +1,151 @@
-console.log("test")
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatableInput: Validatable){
+    let isValid = true;
+    if(validatableInput.required){
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if(
+        validatableInput.minLength != null && 
+        typeof validatableInput.value === "string"
+    ) {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if(
+        validatableInput.maxLength != null && 
+        typeof validatableInput.value === "string"
+    ) {
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if(
+        validatableInput.min != null && 
+        typeof validatableInput.value === "number"
+    ) {
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if(
+        validatableInput.max != null && 
+        typeof validatableInput.value === "number"
+    ) {
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+}
+
+// autobind decorator
+function autobind(
+    target:any, 
+    methodName:string, 
+    descriptor:PropertyDescriptor
+    ){
+        const originalMethod = descriptor.value;
+        const adjDescriptor: PropertyDescriptor = {
+            configurable: true,
+            get(){
+                const boundFn = originalMethod.bind(this);
+                return boundFn
+            }
+        }
+        return adjDescriptor;
+    }
+
+// ProjectInput class
+    class ProjectInput{
+    templateElement:HTMLTemplateElement;
+    hostElement:HTMLDivElement;
+    element:HTMLFormElement;
+    titleInputElement:HTMLFormElement;
+    descriptionInputElement:HTMLFormElement;
+    mandayInputElement:HTMLFormElement;
+
+    constructor(){
+        this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+        this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = "user-input";
+
+        this.titleInputElement = this.element.querySelector(
+            "#title"
+        ) as HTMLFormElement;
+        this.descriptionInputElement = this.element.querySelector(
+            "#description"
+        ) as HTMLFormElement;
+        this.mandayInputElement = this.element.querySelector(
+            "#manday"
+        ) as HTMLFormElement;
+
+        this.configure();
+        this.attach();
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredManday = this.mandayInputElement.value;
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true
+        };
+
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        };
+
+        const mandayValidatable: Validatable = {
+            value: enteredManday,
+            required: true,
+            min: 1,
+            max:100
+        };
+
+        if(
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(mandayValidatable)
+        ){
+            alert("入力値が正しくありません。再度お試しください。")
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, enteredManday]
+        }
+    }
+
+    private clearInputs(){
+        this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.mandayInputElement.value = "";
+    }
+
+    @autobind
+    private submitHandler(event:Event){
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if(Array.isArray(userInput)){
+            const [title, desc, manday] = userInput;
+            console.log(title, desc, manday);
+            this.clearInputs();
+        }
+    }
+
+    private configure(){
+        this.element.addEventListener("submit", this.submitHandler)
+    }
+
+    private attach(){
+        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    }
+}
+
+const prjInput = new ProjectInput();
